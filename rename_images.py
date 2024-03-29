b5bc0f4175dde6c1aa0f9081e4fe1a848b1ed7c8
NAME <EMAIL>
2024-03-29 16:09:02 +0000

    many updates

--- rename_images.py
+++ rename_images.py
@@ -2,19 +2,46 @@ import os
 import shutil
 import glob
 import subprocess
+import logging
+
 from PIL import Image
 from io import BytesIO
 from ollama import generate
 import face_recognition
 import pickle
 import math
+import json
+
+# ANSI escape sequences for different colors
+class ColoredFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    green = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    white = "\x1b[37;20m"
+
+    FORMATS = {
+        logging.DEBUG: grey,
+        logging.INFO: green,
+        logging.WARNING: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: bold_red
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(f"{self.grey}%(asctime)s{self.reset} - {log_fmt}%(levelname)s{self.reset} - {self.white}%(message)s{self.reset}", "%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+
+# Configure logging with color
+handler = logging.StreamHandler()
+handler.setFormatter(ColoredFormatter())
+logging.basicConfig(level=logging.INFO, handlers=[handler])
 
 PROMPT = """
-### User: Make a filename for this image in six words or less including any names from this description.
-The filename should be all lowercase with underscores instead of spaces and contain only alphabetic
-characters. The goal is to make a reasonably short descriptive filename for the image.
-
-Image description:
+### User: Rename this image...
 """
 
 def load_known_faces(known_faces_file):
@@ -22,8 +49,11 @@ def load_known_faces(known_faces_file):
         known_faces = pickle.load(f)
     return known_faces['encodings'], known_faces['names']
 
-def get_image_files(folder_path):
-    return glob.glob(f"{folder_path}/*.png") + glob.glob(f"{folder_path}/*.jpg")
+def get_image_files(folder_path, glob_pattern):
+    if glob_pattern:
+        return glob.glob(f"{folder_path}/{glob_pattern}")
+    else:
+        return glob.glob(f"{folder_path}/*.png") + glob.glob(f"{folder_path}/*.jpg")
 
 def recognize_faces(image_file, known_face_encodings, known_face_names):
     image = face_recognition.load_image_file(image_file)
@@ -36,26 +66,49 @@ def recognize_faces(image_file, known_face_encodings, known_face_names):
             recognized_names.append(known_face_names[first_match_index])
     return recognized_names
 
-def generate_new_name(image_file, recognized_names):
+def normalize_and_convert_image(image_file):
+    # Extract the file extension
+    _, file_extension = os.path.splitext(image_file)
+
     with Image.open(image_file) as img:
+        width, height = img.size
+        new_width = 320
+        new_height = int(height * (new_width / width))
+        img_resized = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
         with BytesIO() as buffer:
-            img.save(buffer, format='PNG')
+            img_resized.save(buffer, format='PNG')
             image_bytes = buffer.getvalue()
 
-    # Llava just ignores prompts and describes the image,
-    options = { 'temperature': 0 }
-    llava = generate(model='llava:latest', prompt="Give a detailed description of the image", images=[image_bytes], options=options, stream=False)
+    return image_bytes, file_extension
 
-    prompt = PROMPT + llava['response'][:500] + "### Assistant:"
+def generate_new_name(image_file, recognized_names):
+    image_bytes, file_extension = normalize_and_convert_image(image_file)
+    base_name = os.path.basename(image_file)
 
+    prompt = f"{PROMPT}\nFilename: {base_name}\n"
     if recognized_names:
         prompt += ' ' + ', '.join(recognized_names)
 
-    message = generate(model='mistral:latest', prompt=prompt, images=[image_bytes], options=options, stream=False)
-    new_name = message['response'].strip().replace(' ', '_') + '.png'
-    if not new_name.endswith('.png'):
-        new_name += '.png'
-    return new_name
+    options = { 'temperature': 0 }
+    message = generate(model='phlava:latest', prompt=prompt, images=[image_bytes], options=options, format='json', stream=False)
+
+    if 'total_duration' in message:
+        logging.info(f"Total duration for {base_name}: {message['total_duration']}")
+    if 'eval_count' in message:
+        logging.info(f"Evaluation count for {base_name}: {message['eval_count']}")
+
+    response_json = json.loads(message['response'])
+    try:
+        filename = response_json['filename']
+        description = response_json['description']
+    except Exception as error:
+        logging.error(f"ERROR: {error}")
+
+    # Ensure the filename ends with file extension
+    if not filename.endswith(file_extension):
+        filename += file_extension
+
+    return filename, description
 
 def shannon_entropy(data):
     if not data:
@@ -66,16 +119,19 @@ def shannon_entropy(data):
         entropy += - p_x * math.log2(p_x)
     return entropy
 
-def rename_images(source_dir):
+def rename_images(source_dir, glob_pattern):
     known_faces_file = 'known_faces.dat' # Hardcoded path to the known faces file
     known_face_encodings, known_face_names = load_known_faces(known_faces_file)
-    for img in get_image_files(source_dir):
+    for img in get_image_files(source_dir, glob_pattern):
         original_name = os.path.basename(img)
         entropy = shannon_entropy(original_name)
         if entropy < 3.5:
             continue
         recognized_names = recognize_faces(img, known_face_encodings, known_face_names)
-        new_name = generate_new_name(img, recognized_names)
+        if recognized_names:
+            logging.info(f"Known faces: {str(recognized_names)}")
+
+        new_name, comment = generate_new_name(img, recognized_names)
         base_name = new_name
         count = 1
         while os.path.exists(base_name):
@@ -84,16 +140,22 @@ def rename_images(source_dir):
 
         shutil.copy(img, base_name)
 
-        # Add the old name as a comment to the new file using exiftool
+        # Add the old name, people and description as a comment to the new file using exiftool
         old_name = os.path.basename(img)
-        subprocess.run(['exiftool', '-Comment=' + old_name, base_name, '-overwrite_original'])
+        logging.info(f"Renaming from {old_name} to {new_name}")
 
-# Example usage
+        comment = f"-Comment=Filename:{old_name} People:{', '.join(recognized_names)} Details: {comment}"
+        logging.info(f"Comment: {comment}")
+
+        result = subprocess.run(['exiftool', comment, base_name, '-overwrite_original'], capture_output=True, text=True)
+        logging.info(f"Exiftool: {base_name}: {result.stdout.strip()}")
+
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 2:
-        print("Usage: python script_name.py <source_directory>")
+    if len(sys.argv) < 3:
+        print("Usage: python script_name.py <source_directory> <glob_pattern>")
         sys.exit(1)
 
     source_dir = sys.argv[1]
-    rename_images(source_dir)
+    glob_pattern = sys.argv[2]
+    rename_images(source_dir, glob_pattern)
