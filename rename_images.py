59ed4529700fa60c51e057c260c0cd019a15018c
NAME <EMAIL>
2024-03-31 14:30:28 +0100

    refactor tidy up

--- rename_images.py
+++ rename_images.py
@@ -1,20 +1,21 @@
 import os
-import shutil
+import sys
 import glob
+import math
+import json
+import shutil
 import subprocess
+import pickle
+import face_recognition
 import logging
 
 from PIL import Image
 from io import BytesIO
 from ollama import generate
-import face_recognition
-import pickle
-import math
-import json
 
-# ANSI escape sequences for different colors
+# Configure logging with color
 class ColoredFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
+    light_magenta = "\x1b[35;20m" # Subdued pink
     green = "\x1b[32;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
@@ -23,7 +24,7 @@ class ColoredFormatter(logging.Formatter):
     white = "\x1b[37;20m"
 
     FORMATS = {
-        logging.DEBUG: grey,
+        logging.DEBUG: green,
         logging.INFO: green,
         logging.WARNING: yellow,
         logging.ERROR: red,
@@ -32,10 +33,9 @@ class ColoredFormatter(logging.Formatter):
 
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(f"{self.grey}%(asctime)s{self.reset} - {log_fmt}%(levelname)s{self.reset} - {self.white}%(message)s{self.reset}", "%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter(f"{self.light_magenta}%(asctime)s{self.reset} - {log_fmt}%(levelname)s{self.reset} - {self.white}%(message)s{self.reset}", "%Y-%m-%d %H:%M:%S")
         return formatter.format(record)
 
-# Configure logging with color
 handler = logging.StreamHandler()
 handler.setFormatter(ColoredFormatter())
 logging.basicConfig(level=logging.INFO, handlers=[handler])
@@ -70,16 +70,25 @@ def normalize_and_convert_image(image_file):
     # Extract the file extension
     _, file_extension = os.path.splitext(image_file)
 
-    with Image.open(image_file) as img:
-        width, height = img.size
+    with Image.open(image_file) as full_image_path:
+        width, height = full_image_path.size
         new_width = 320
         new_height = int(height * (new_width / width))
-        img_resized = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
+        full_image_path_resized = full_image_path.resize((new_width, new_height), Image.Resampling.LANCZOS)
         with BytesIO() as buffer:
-            img_resized.save(buffer, format='PNG')
+            full_image_path_resized.save(buffer, format='PNG')
             image_bytes = buffer.getvalue()
 
     return image_bytes, file_extension
+
+def shannon_entropy(data):
+    if not data:
+        return 0
+    entropy = 0
+    for x in set(data):
+        p_x = data.count(x) / len(data)
+        entropy += - p_x * math.log2(p_x)
+    return entropy
 
 def generate_new_name(image_file, recognized_names):
     image_bytes, file_extension = normalize_and_convert_image(image_file)
@@ -106,42 +115,34 @@ def generate_new_name(image_file, recognized_names):
 
     # Ensure the filename ends with file extension
     if not filename.endswith(file_extension):
-        filename += file_extension
+        filename += file_extension.lower()
 
     return filename, description
 
-def shannon_entropy(data):
-    if not data:
-        return 0
-    entropy = 0
-    for x in set(data):
-        p_x = data.count(x) / len(data)
-        entropy += - p_x * math.log2(p_x)
-    return entropy
-
-def rename_images(source_dir, glob_pattern):
+def rename_images(source_dir, glob_pattern, threshold=3.6):
     known_faces_file = 'known_faces.dat' # Hardcoded path to the known faces file
     known_face_encodings, known_face_names = load_known_faces(known_faces_file)
-    for img in get_image_files(source_dir, glob_pattern):
-        original_name = os.path.basename(img)
+    for full_image_path in get_image_files(source_dir, glob_pattern):
+        original_name = os.path.basename(full_image_path)
         entropy = shannon_entropy(original_name)
-        if entropy < 3.5:
+        if entropy < threshold:
             continue
-        recognized_names = recognize_faces(img, known_face_encodings, known_face_names)
+
+        recognized_names = recognize_faces(full_image_path, known_face_encodings, known_face_names)
         if recognized_names:
             logging.info(f"Known faces: {str(recognized_names)}")
 
-        new_name, comment = generate_new_name(img, recognized_names)
+        new_name, comment = generate_new_name(full_image_path, recognized_names)
         base_name = new_name
         count = 1
         while os.path.exists(base_name):
             base_name = f"{new_name[:-4]}_{count}{new_name[-4:]}"
             count += 1
 
-        shutil.copy(img, base_name)
+        shutil.copy(full_image_path, base_name)
 
         # Add the old name, people and description as a comment to the new file using exiftool
-        old_name = os.path.basename(img)
+        old_name = os.path.basename(full_image_path)
         logging.info(f"Renaming from {old_name} to {new_name}")
 
         comment = f"-Comment=Filename:{old_name} People:{', '.join(recognized_names)} Details: {comment}"
@@ -151,11 +152,11 @@ def rename_images(source_dir, glob_pattern):
         logging.info(f"Exiftool: {base_name}: {result.stdout.strip()}")
 
 if __name__ == "__main__":
-    import sys
     if len(sys.argv) < 3:
-        print("Usage: python script_name.py <source_directory> <glob_pattern>")
+        print("Usage: python script_name.py <source_directory> <glob_pattern> [threshold]")
         sys.exit(1)
 
     source_dir = sys.argv[1]
     glob_pattern = sys.argv[2]
-    rename_images(source_dir, glob_pattern)
+    threshold = float(sys.argv[3]) if len(sys.argv) > 3 else None
+    rename_images(source_dir, glob_pattern, threshold)
